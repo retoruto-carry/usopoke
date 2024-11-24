2e5c782b8c01fac00a9b0231093fc28de562b568
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2024-11-24 14:37:49 +0900

    chore: pokepoke-app用にsupabseを別でセットアップ

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -2,7 +2,7 @@
 // by running `wrangler types`
 
 interface Env {
-	SUPABASE_URL: "https://lyfolpqxqktrbmnuhghc.supabase.co";
-	SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5Zm9scHF4cWt0cmJtbnVoZ2hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzODczODUsImV4cCI6MjA0Mzk2MzM4NX0.T7-Y6q3tBmfDWDq5scg1bMK7HA5j49tHxrPywr6YzrI";
+	SUPABASE_URL: "https://bppgbaxsswwmvfxabgmk.supabase.co";
+	SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwcGdiYXhzc3d3bXZmeGFiZ21rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0MjU4NTAsImV4cCI6MjA0ODAwMTg1MH0.FpMNmi6WIcFmvDzp0AkPam2Vxe7D7wurKNfe2V07A_c";
 	LAST_FM_API_KEY: string;
 }
