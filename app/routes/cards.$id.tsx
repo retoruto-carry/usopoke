c62f5b809216f44f9ebd979a2e13b0a03343bb4b
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2024-12-08 15:15:31 +0900

    fix: sgit st

--- app/routes/cards.$id.tsx
+++ app/routes/cards.$id.tsx
@@ -1,8 +1,9 @@
 import { LoaderFunction, json } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { createServerSupabase } from "~/utils/supabase.server";
 import type { Database } from "~/types/supabase";
 import Draw from "~/components/domain/draw";
+import { Card3 } from "~/components/domain/card/card3/Card";
 
 type LoaderData = {
   card: Database["public"]["Tables"]["card_images"]["Row"];
@@ -26,12 +27,17 @@ export const loader: LoaderFunction = async ({ request, context, params }) => {
   return json<LoaderData>({ card });
 };
 
+const CARD_WIDTH = 360;
+const CARD_HEIGHT = CARD_WIDTH * 1.4;
+
 export default function Card() {
   const { card } = useLoaderData<LoaderData>();
 
   return (
     <div className="max-w-md mx-auto p-4 min-h-screen bg-gray-50">
-      <h1 className="text-2xl text-purple-600 mb-4">うそポケ画像メーカー</h1>
+      <Link to="/">
+        <h1 className="text-2xl text-purple-600 mb-4">うそポケ画像メーカー</h1>
+      </Link>
 
       <div className="bg-purple-400 p-4 text-white text-center mb-4">
         パックの開封結果
@@ -39,13 +45,19 @@ export default function Card() {
 
       <div className="bg-white p-4 rounded-lg shadow-md">
         <div className="space-y-4">
-          <div className="bg-white rounded-lg shadow-sm p-4">
-            <img
-              src={card.image_url}
-              alt="カード"
-              className="w-full rounded-lg"
-              loading="lazy"
-            />
+          <div className="bg-white rounded-lg shadow-sm p-4 flex justify-center">
+            <div>
+              <Card3 width={CARD_WIDTH}>
+                <img
+                  width={CARD_WIDTH}
+                  height={CARD_HEIGHT}
+                  src={card.image_url}
+                  alt="カード"
+                  className="w-full rounded-lg"
+                  loading="lazy"
+                />
+              </Card3>
+            </div>
           </div>
 
           <div className="flex justify-between items-center">
