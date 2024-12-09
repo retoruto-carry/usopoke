4e6671be99a672bd531f17031fb5274877ed41c1
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2024-12-10 02:49:51 +0900

    feat: フッターに宣伝追加

--- app/components/common/AppFooter.tsx
+++ app/components/common/AppFooter.tsx
@@ -1,13 +1,15 @@
 import { Link } from "@remix-run/react";
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+import { ClientOnly } from "remix-utils/client-only";
 
 export const AppFooter = () => {
   return (
     <div className="text-center text-xs text-gray-500 leading-relaxed mt-16">
       <p className="mb-2 text-white">任天堂さんとは一切関係ありません。完全非公式のネタサービスです</p>
-      <p className="mb-2 text-white">
-        作った人: <a href="https://x.com/retoruto_carry" className="border-b border-gray-500 border-dotted" target="_blank" rel="noreferrer">@retoruto_carry</a>
+      <p className="mb-2 text-white font-medium text-md">
+        開発した人: <a href="https://x.com/retoruto_carry" className="border-b border-gray-500 border-dotted" target="_blank" rel="noreferrer">@retoruto_carry</a>
       </p>
-      <p className="mb-2 text-white">
+      <p className="mb-2 text-white font-medium text-md">
         絵を描いた人: <a href="https://x.com/kiyonagarin" className="border-b border-gray-500 border-dotted" target="_blank" rel="noreferrer">@kiyonagarin</a>
       </p>
       <Link to="/">
@@ -16,6 +18,17 @@ export const AppFooter = () => {
           <h1 className="text-2xl text-white font-bold">うそポケ画像メーカー</h1>
         </div>
       </Link>
+      <div className="mt-10">
+        <p className="text-white font-bold text-center text-lg">↓こんなサービスも作ってます</p>
+        <p className="text-white text-center text-xs mt-2">RTお願いします🙏</p>
+        <div className="items-center justify-center mt-4 w-full">
+          <ClientOnly>
+            {() => <TwitterTweetEmbed
+              tweetId={'1796123557090517067'}
+            />}
+          </ClientOnly>
+        </div>
+      </div>
     </div>
   );
 };
