d37f87aeadcdc61c5dce2b020e42c7ed281ab9f9
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2024-12-10 02:35:42 +0900

    fix: textare style

--- app/components/common/TextareaAutoResize.tsx
+++ app/components/common/TextareaAutoResize.tsx
@@ -12,7 +12,7 @@ const TextareaAutoResize = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <TextareaAutosize
         className={cn(
-          "flex w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "flex w-full border border-input bg-gray-50 px-3 py-2 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           className
         )}
         ref={ref}
