f724402c6bc548c403df254a5436ed8e74c96c28
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2024-12-23 22:52:18 +0900

    add: google ads meta

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-adsense-account" content="ca-pub-7293616767912550" />
         <Meta />
         <Links />
       </head>
